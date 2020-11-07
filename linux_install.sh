b905d5a6a974a2f7eef36c0dd889cbbec90f8bf2
cyberbonbon <70076544+cyberbonbon@users.noreply.github.com>
2020-11-07 16:32:45 -0500

    Update linux_install.sh

--- linux_install.sh
+++ linux_install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo touch /etc/apt/apt.conf.d/01proxy
+sudo touch /etc/apt/apt.conf.d/02proxy
 #sudo echo 'Acquire::http::Proxy “http://morannon.gondor.middleearth.network:3142”;' > /etc/apt/apt.conf.d/01proxy
